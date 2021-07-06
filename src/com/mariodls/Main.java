f1dd82c8fa65a2ee63a82006de297bc48d3366fd
Marios Diles <marios.diles@gmail.com>
2021-07-06 16:58:00 +0300

    added eat(), move() in Animal Class,  added Dog Class with own props, added new Animal, and new Dog in Main, and called eat()

--- src/com/mariodls/Main.java
+++ src/com/mariodls/Main.java
@@ -1,7 +1,15 @@
 package com.mariodls;
 
+import com.mariodls.com.mariodls.Animal;
+import com.mariodls.com.mariodls.com.mariodls.Dog;
+
 public class Main {
     public static void main(String[] args) {
+
+        Animal snakeKolov = new Animal("Kolovo",1,1,1,2);
+
+        Dog gigi = new Dog("Gigi", 2,15,2,4,1,20,"silky");
+        gigi.eat();
 
     }
 }
--- src/com/mariodls/com/mariodls/Animal.java
+++ src/com/mariodls/com/mariodls/Animal.java
@@ -16,6 +16,14 @@ public class Animal {
         this.weight = weight;
     }
 
+    public void eat() {
+        System.out.println("Animal.eat() called");
+    }
+
+    public void move() {
+
+    }
+
     public String getName() {
         return name;
     }
